ecd14162ad0f56c65b440ce4e67fce0373481790
Rafael Atheya <rafael.satriabhi@gmail.com>
2020-09-18 15:13:38 +0700

    Semangat gaess

--- app.js
+++ app.js
@@ -8,69 +8,35 @@ app.use(cors());
 const wordGenerator = require('./helpers/wordgen.js');
 //tinggal panggil wordGenerator()
 //returning array of 10 random words
-let dataUser = {};
+let dataUsers = [];
 //v-for {{},{},{}} data in datas // data.score
 let id;
 io.on('connection', (socket) => {
   id = socket.id
   console.log('a user connected')
-  dataUser[`${socket.id}`] = {
-    id: socket.id,
-    username: '',
-    score: 0
-  }
-  socket.emit('GET_DATA_USER', {dataUser, id})
-
-  //creating instance of connected user
-  socket.emit('GET_LIST_QUESTION', wordGenerator())
+  
+  io.emit('GET_LIST_QUESTION', wordGenerator())
 
   socket.on('newUser', data => {
-    dataUser[socket.id].username = data.username
-    socket.emit('GET_DATA_USER', {dataUser, id})
+    dataUsers.push({
+      id: id,
+      name: data,
+      score: 0
+    })
+    console.log(dataUsers)
+    io.emit('GET_DATA_USER', dataUsers)
   })
   //set the username of instance user
 
-  socket.on('getScore', data => {
-    console.log('------------------')
-    dataUser[socket.id].score = data
-    console.log(dataUser)
-    socket.emit('GET_DATA_USER', {dataUser, id})
+  socket.on('incrementScore', data => {
+    dataUsers.forEach((elem)=>{
+      if(elem.name===data){
+        elem.score += 10
+      }
+    console.log(dataUsers)
+      io.emit('GET_DATA_USER', dataUsers)
+    })
   })
-  // socket.on('endOfGame', data => {
-  //   dataUser = {}
-  //   socket.emit('GET_DATA_USER',dataUser)
-  // })
-  //listening getScore action from client to get scoring
-
-  socket.on('disconnect', () => {
-    socket.disconnect(true);
-  })
-
-  console.log(`${dataUser[`${id}`].id} has joinned`)
-  //creating instance of connected user
-  // console.log(dataUser[socket.id])
-  // socket.on('getUsername', data => {
-  //   dataUser[socket.id].username = data
-  // })
-  //set the username of instance user
-
-  // socket.on('getScore',data =>{
-  //   dataUser[socket.id].score =  data
-  // })
-  //listening getScore action from client to get scoring
-
-  // socket.on('returnFinalData', data => {
-  //   dataUser
-  // })
-  // socket.on('disconnect',socket => {
-  //   // console.log(dataUser[`${id}`].id)
-  //   console.log(`${dataUser[`${id}`].id} has been disconnected`)
-  //   delete dataUser[`${id}`]
-  //   console.log('user tersisa')
-  //   console.log(dataUser)
-  //
-  // })
-  // deleting user when disconnected
 })
 
 http.listen(PORT, () => {
